56fbc74a47a05db46ad901141b4026c48937f199
Erecik <eret112@gmail.com>
2021-10-01 00:11:13 +0200

    Added filtrs and book table

--- book/views.py
+++ book/views.py
@@ -1,23 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from book.models import Book
 from book.forms import BookForm
+from django.db.models import Q
+
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
 
 
 def book_list(request):
-    books = Book.objects.all()
-    form = BookForm()
-    if request.method == "POST":
-        query_name = request.POST.get('name', None)
-        if query_name:
-            results = Book.objects.filter()
-            return render(request, 'index.html', {"results": results})
-
+    book_filter = Q()
+    title = request.GET.get('title')
+    author = request.GET.get('author')
+    language = request.GET.get('language')
+    start_date = request.GET.get('startdate')
+    end_date = request.GET.get('enddate')
+    if is_valid_queryparam(title):
+        book_filter &= Q(title__icontains=title)
+    if is_valid_queryparam(author):
+        book_filter &= Q(author__icontains=author)
+    if is_valid_queryparam(language):
+        book_filter &= Q(language=language)
+    if is_valid_queryparam(start_date):
+        book_filter &= Q(date_of_publication__gte=start_date)
+    if is_valid_queryparam(end_date):
+        book_filter &= Q(date_of_publication__lte=end_date)
+    if is_valid_queryparam(book_filter):
+        books = Book.objects.filter(book_filter, )
+    else:
+        books = Book.objects.all()
     return render(request, 'index.html',
-                  {'books': books, 'form': form})
+                  {'books': books})
+
 
 def add_book(request):
     form = BookForm()
-    if request.method == "POST":
+    if request.POST:
+        form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'index.html')
+            return redirect(book_list)
+    return render(request, 'add_book.html', {'form': form})
