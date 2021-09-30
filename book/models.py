0a1e7bbc658d4426368b58d58744b6de74d5110f
Erecik <eret112@gmail.com>
2021-10-01 00:12:16 +0200

    Changed book fields language to english

--- book/models.py
+++ book/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
-    tytul = models.CharField(max_length=32)
-    autor = models.CharField(max_length=64)
-    data_publikacji = models.DateField()
-    numer_ISBN = models.CharField(max_length=17)
-    liczba_stron = models.IntegerField()
-    link_do_okladki = models.URLField(max_length=200)
-    jezyk_publikacji = models.CharField(max_length=32)
+    title = models.CharField(max_length=32)
+    author = models.CharField(max_length=64)
+    date_of_publication = models.DateField()
+    ISBN_number = models.CharField(max_length=17)
+    number_of_pages = models.IntegerField()
+    link_to_cover = models.URLField(max_length=200)
+    language = models.CharField(max_length=32)
 
     def __str__(self):
-        return self.tytul
+        return self.title
