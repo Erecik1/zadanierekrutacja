725fc77c75594653d6cdeccc7604fa6fdd101f6b
Erecik <eret112@gmail.com>
2021-10-01 00:12:44 +0200

    Added url to add new books

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from book.views import book_list
+from book.views import book_list, add_book
 
 urlpatterns = [
-    path('', book_list),
+    path('', book_list, name="index"),
+    path('add/', add_book),
 ]
