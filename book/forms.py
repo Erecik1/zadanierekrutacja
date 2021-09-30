c786771304aef7de6271bb1232b7107a9a281fbc
Erecik <eret112@gmail.com>
2021-10-01 00:13:12 +0200

    Added attr to fields

--- book/forms.py
+++ book/forms.py
@@ -7,3 +7,11 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+
+        widgets = {"title": forms.widgets.TextInput(attrs={"class": "form-control"}),
+                   "author": forms.widgets.TextInput(attrs={"class": "form-control"}),
+                   "date_of_publication": forms.widgets.DateInput(attrs={"class": "form-control", "type": "date"}),
+                   "ISBN_number": forms.widgets.TextInput(attrs={"class": "form-control"}),
+                   "number_of_pages": forms.widgets.TextInput(attrs={"class": "form-control"}),
+                   "link_to_cover": forms.widgets.TextInput(attrs={"class": "form-control"}),
+                   "language": forms.widgets.TextInput(attrs={"class": "form-control"}), }
