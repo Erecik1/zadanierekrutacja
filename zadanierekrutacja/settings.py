2632153c89199dad0ca07fbcee76ab38c4f2210e
Erecik <eret112@gmail.com>
2021-10-01 00:09:58 +0200

    Added comment about leaked key

--- zadanierekrutacja/settings.py
+++ zadanierekrutacja/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Wiem ze klucz nie powinnien trafic w takiej formie na publiczne repo jednak z uznalem ze nie jest to az tak potrzebne
+# by eksportowac klucz do innego pliku na potrzeby tego zadania
 SECRET_KEY = 'django-insecure-!#ud3%rll@v)=yti0go!(17dzb%5zqact)%hj!yubkri5xy^-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
