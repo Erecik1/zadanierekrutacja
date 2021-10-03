3203445e3a3f3ce6d2cba0f5b1501c2a1d3f9403
Erecik <eret112@gmail.com>
2021-10-04 01:58:29 +0200

    Added book model to admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from book.models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display =  ('title')
+    search_fields = ('title')
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Book)
