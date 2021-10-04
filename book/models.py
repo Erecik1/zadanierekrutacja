e01da40b67c36d4f1e4291dae962a77c05e6bee4
Erecik <eret112@gmail.com>
2021-10-04 02:00:10 +0200

    Changed all fields to nullable

--- book/models.py
+++ book/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=32)
-    author = models.CharField(max_length=64)
-    date_of_publication = models.DateField()
-    ISBN_number = models.CharField(max_length=17)
-    number_of_pages = models.IntegerField()
-    link_to_cover = models.URLField(max_length=200)
-    language = models.CharField(max_length=32)
+    author = models.CharField(max_length=64, null=True)
+    date_of_publication = models.DateField(null=True)
+    ISBN_number = models.CharField(max_length=17, null=True)
+    number_of_pages = models.IntegerField(blank=True, null=True)
+    link_to_cover = models.URLField(max_length=200, blank=True, null=True)
+    language = models.CharField(max_length=32, null=True)
 
     def __str__(self):
         return self.title
