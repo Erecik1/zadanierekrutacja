f1e65ad2e44527dfa709b4d64fc13d67548f5845
Erecik <eret112@gmail.com>
2021-10-04 13:51:07 +0200

    Changed debug to False

--- zadanierekrutacja/settings.py
+++ zadanierekrutacja/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!#ud3%rll@v)=yti0go!(17dzb%5zqact)%hj!yubkri5xy^-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
