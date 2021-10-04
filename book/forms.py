eabb330628cce50cdbf0de99b0de0b6b0dbc3ae9
Erecik <eret112@gmail.com>
2021-10-04 02:01:34 +0200

    Added form to import books

--- book/forms.py
+++ book/forms.py
@@ -15,3 +15,14 @@ class BookForm(forms.ModelForm):
                    "number_of_pages": forms.widgets.TextInput(attrs={"class": "form-control"}),
                    "link_to_cover": forms.widgets.TextInput(attrs={"class": "form-control"}),
                    "language": forms.widgets.TextInput(attrs={"class": "form-control"}), }
+
+
+class BookImportForm(forms.Form):
+    form_widget = forms.widgets.TextInput(attrs={"class": "form-control"})
+    intitle = forms.CharField(required=False, widget=form_widget)
+    inauthor = forms.CharField(required=False, widget=form_widget)
+    inpublisher = forms.CharField(required=False, widget=form_widget)
+    subject = forms.CharField(required=False, widget=form_widget)
+    isbn = forms.CharField(required=False, widget=form_widget)
+    lccn = forms.CharField(required=False, widget=form_widget)
+    oclc = forms.CharField(required=False, widget=form_widget)
